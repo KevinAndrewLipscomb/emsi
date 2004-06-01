46fc747c7c423e1c33a810dac0f2d0d757f28202
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2004-06-01 22:09:26 +0000

    Recovered from in-house repository, since some of these Tafkap memos are still referenced on the site.

---
+++ trunk/emsi/web/html/resources/TafkapMemos/2002-004/layout.php
@@ -0,0 +1,22 @@
+<?php
+virtual("/std-header.html");
+virtual("/std-pre-content.html");
+?>
+<table cellspacing="0" cellpadding="5">
+   <tr>
+      <td>
+         <img border="0" src="/images/wpe1.gif" width="128" height="128">
+      </td>
+      <td>
+         <img border="0" src="/images/tafkap.gif">-2002-004
+      </td>
+   </tr>
+</table>
+&nbsp;
+<p><img border="0" src="2002-004-a.gif"></p>
+<p><img border="0" src="2002-004-b.gif"><br>
+<img border="0" src="2002-004-c.gif"><br>
+<img border="0" src="2002-004-d.gif">
+<?php
+virtual("/std-post-content.html");
+?>
---
+++ trunk/emsi/web/html/resources/TafkapMemos/2003-002/layout.php
@@ -0,0 +1,22 @@
+<?php
+virtual("/std-header.html");
+virtual("/std-pre-content.html");
+?>
+<table cellspacing="0" cellpadding="5">
+   <tr>
+      <td>
+         <img border="0" src="/images/wpe1.gif" width="128" height="128">
+      </td>
+      <td>
+         <img border="0" src="/images/tafkap.gif" width="13" height="15">-2002-003
+      </td>
+   </tr>
+</table>
+&nbsp;
+<p><img border="0" src="a.gif" width="500" height="70"></p>
+<p><img border="0" src="b.gif" width="798" height="624"></p>
+<p><img border="0" src="c.gif" width="779" height="973"><br>
+<img border="0" src="d.gif" width="787" height="260"></p>
+<?php
+virtual("/std-post-content.html");
+?>
