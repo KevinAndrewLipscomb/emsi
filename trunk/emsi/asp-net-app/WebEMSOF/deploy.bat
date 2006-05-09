c3fc7afdfb83b8f574f29e2604ce90223c939aea
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2006-05-09 16:29:28 +0000

    Created work.bat.  Revised deploy.bat to use a suboptimal method that does not rely on the unreliable Netdrive.exe.  The combination of work.bat and deploy.bat now allows for a deployment process that involves a trivial drag-and-drop step.

--- trunk/emsi/asp-net-app/WebEMSOF/deploy.bat
+++ trunk/emsi/asp-net-app/WebEMSOF/deploy.bat
@@ -1,9 +1,10 @@
 @REM
 @REM $Id$
 @REM
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\*.asax x:\ssl\WebEMSOF /d /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\*.aspx x:\ssl\WebEMSOF /d /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\bin\*.dll x:\ssl\WebEMSOF\bin /d /i /k /y /z
-@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Data.Provider.dll" x:\ssl\WebEMSOF\bin /d /i /k /y /z
-@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Data.Common.dll" x:\ssl\WebEMSOF\bin /d /i /k /y /z
-@xcopy "C:\Program Files\CoreLab\Bdp\MySql\BDS4\CoreLab.Bdp.MySql.dll" x:\ssl\WebEMSOF\bin /d /i /k /y /z
+@rmdir ready-to-deploy /s /q
+@xcopy C:\Inetpub\wwwroot\WebEMSOF\*.asax ready-to-deploy /m /i /k /y /z
+@xcopy C:\Inetpub\wwwroot\WebEMSOF\*.aspx ready-to-deploy /m /i /k /y /z
+@xcopy C:\Inetpub\wwwroot\WebEMSOF\bin\*.dll ready-to-deploy\bin /m /i /k /y /z
+@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Data.Provider.dll" ready-to-deploy\bin /m /i /k /y /z
+@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Data.Common.dll" ready-to-deploy\bin /m /i /k /y /z
+@xcopy "C:\Program Files\CoreLab\Bdp\MySql\BDS4\CoreLab.Bdp.MySql.dll" ready-to-deploy\bin /m /i /k /y /z
