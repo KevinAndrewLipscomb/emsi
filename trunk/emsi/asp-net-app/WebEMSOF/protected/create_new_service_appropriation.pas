3b36da864a139c63a561dc18c407162ff8f59c1e
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2006-05-22 18:20:06 +0000

    Revised code to use CustomValidator to prevent new service appropriations from exceeded amount remaining in county appropriation.

--- trunk/emsi/asp-net-app/WebEMSOF/protected/create_new_service_appropriation.pas
+++ trunk/emsi/asp-net-app/WebEMSOF/protected/create_new_service_appropriation.pas
@@ -21,6 +21,7 @@ type
       e: System.EventArgs);
     procedure CheckBox_unfilter_CheckedChanged(sender: System.Object; e: System.EventArgs);
     procedure LinkButton_logout_Click(sender: System.Object; e: System.EventArgs);
+    procedure CustomValidator_amount_ServerValidate(source: System.Object; args: System.Web.UI.WebControls.ServerValidateEventArgs);
   {$ENDREGION}
   strict private
     procedure Page_Load(sender: System.Object; e: System.EventArgs);
@@ -52,6 +53,7 @@ type
     Label_unappropriated_amount: System.Web.UI.WebControls.Label;
     TableRow_sum_of_service_appropriations: System.Web.UI.HtmlControls.HtmlTableRow;
     TableRow_unappropriated_amount: System.Web.UI.HtmlControls.HtmlTableRow;
+    CustomValidator_amount: System.Web.UI.WebControls.CustomValidator;
     procedure OnInit(e: EventArgs); override;
   private
   public
@@ -72,6 +74,7 @@ begin
   Include(Self.Button_add_appropriation_and_repeat.Click, Self.Button_add_appropriation_and_repeat_Click);
   Include(Self.Button_add_appropriation_and_stop.Click, Self.Button_add_appropriation_and_stop_Click);
   Include(Self.Button_cancel.Click, Self.Button_cancel_Click);
+  Include(Self.CustomValidator_amount.ServerValidate, Self.CustomValidator_amount_ServerValidate);
   Include(Self.Load, Self.Page_Load);
 end;
 {$ENDREGION}
@@ -119,6 +122,20 @@ begin
   inherited OnInit(e);
 end;
 
+procedure TWebForm_create_new_service_appropriation.CustomValidator_amount_ServerValidate(source: System.Object;
+  args: System.Web.UI.WebControls.ServerValidateEventArgs);
+var
+  amount_string: string;
+begin
+  amount_string := Safe(args.value,REAL_NUM);
+  if amount_string = system.string.EMPTY then begin
+    args.isvalid := FALSE;
+  end else begin
+    amount := decimal.Parse(amount_string);
+    args.isvalid := (amount > 0) and (amount <= unappropriated_amount);
+  end;
+end;
+
 procedure TWebForm_create_new_service_appropriation.LinkButton_logout_Click(sender: System.Object;
   e: System.EventArgs);
 begin
@@ -137,8 +154,10 @@ end;
 procedure TWebForm_create_new_service_appropriation.Button_add_appropriation_and_stop_Click(sender: System.Object;
   e: System.EventArgs);
 begin
-  AddAppropriation;
-  server.Transfer('county_dictated_appropriations.aspx');
+  if page.isvalid then begin
+    AddAppropriation;
+    server.Transfer('county_dictated_appropriations.aspx');
+  end;
 end;
 
 procedure TWebForm_create_new_service_appropriation.Button_cancel_Click(sender: System.Object;
@@ -150,27 +169,26 @@ end;
 procedure TWebForm_create_new_service_appropriation.Button_add_appropriation_and_repeat_Click(sender: System.Object;
   e: System.EventArgs);
 begin
-  //
-  AddAppropriation;
-  TextBox_new_amount.Text := system.string.EMPTY;
-  DropDownList_services.SelectedIndex := -1;
-  //
-  // Update labels in the Parent appropriation section.
-  //
-  Label_sum_of_service_appropriations.text :=
-    (decimal.parse(Safe(Label_sum_of_service_appropriations.text,REAL_NUM)) + amount).tostring('C');
-  unappropriated_amount := unappropriated_amount - amount;
-  Label_unappropriated_amount.text := unappropriated_amount.tostring('C');
-  if unappropriated_amount < 0 then begin
-    Label_unappropriated_amount.font.bold := TRUE;
-    Label_unappropriated_amount.forecolor := color.red;
+  if page.isvalid then begin
+    AddAppropriation;
+    TextBox_new_amount.Text := system.string.EMPTY;
+    DropDownList_services.SelectedIndex := -1;
+    //
+    // Update labels in the Parent appropriation section.
+    //
+    Label_sum_of_service_appropriations.text :=
+      (decimal.parse(Safe(Label_sum_of_service_appropriations.text,REAL_NUM)) + amount).tostring('C');
+    unappropriated_amount := unappropriated_amount - amount;
+    Label_unappropriated_amount.text := unappropriated_amount.tostring('C');
+    if unappropriated_amount < 0 then begin
+      Label_unappropriated_amount.font.bold := TRUE;
+      Label_unappropriated_amount.forecolor := color.red;
+    end;
   end;
-  //
 end;
 
 procedure TWebForm_create_new_service_appropriation.AddAppropriation;
 var
-  amount_string: string;
   bc_get_cc_email_address: borland.data.provider.bdpcommand;
   bc_get_fy_designator: borland.data.provider.bdpcommand;
   bc_get_service_email_address: borland.data.provider.bdpcommand;
@@ -178,101 +196,95 @@ var
   messageText: string;
   service_id_string: string;
 begin
-  amount_string := Safe(TextBox_new_amount.Text.Trim,REAL_NUM);
   service_id_string := Safe(DropDownList_services.SelectedValue,NUM);
-  if amount_string <> system.string.EMPTY then begin
-    amount := decimal.Parse(amount_string);
-    if amount > 0 then begin
-      appcommon.DbOpen;
-      //
-      // Record the new appropriation.
-      //
-      borland.data.provider.bdpcommand.Create
-        (
-        'insert into county_dictated_appropriation'
-        + ' set region_dictated_appropriation_id = ' + session.Item['region_dictated_appropriation_id'].tostring + ','
-        +   ' service_id = ' + service_id_string + ','
-        +   ' amount = ' + amount.tostring + ','
-        +   ' match_level_id = ' + Safe(RadioButtonList_match_level.selectedvalue,NUM),
-        appcommon.db
-        )
-        .ExecuteNonQuery;
-      //
-      // Initialize a new emsof_request_master record, since at this time there must be a one-to-one relationship between a county-
-      // dictated appropriation and an EMSOF request.
-      //
-      //   Get max(county_dictated_appropriation.id), which must be the id of the county_dictated_appropriation record that we just
-      //   inserted.
-      //
-      max_county_dictated_appropriation_id_string := borland.data.provider.bdpcommand.Create
-        (
-        'select max(id) from county_dictated_appropriation',
-        appcommon.db
-        )
-        .ExecuteScalar.tostring;
-      //
-      //    Insert and link back to the above max id.
-      //
-      borland.data.provider.bdpcommand.Create
-        (
-        'insert into emsof_request_master set county_dictated_appropriation_id = ' + max_county_dictated_appropriation_id_string,
-        appcommon.db
-        )
-        .ExecuteNonQuery;
-      //
-      // Send notice of the appropriation to the service's email address of record.
-      //
-      //   Set up the command to get service's email address of record.
-      bc_get_service_email_address := borland.data.provider.bdpcommand.Create
-        (
-        'select password_reset_email_address from service_user '
-        + 'where id = ' + service_id_string,
-        appcommon.db
-        );
-      //   Set up the command to get the appropriate fiscal year designator.
-      bc_get_fy_designator := borland.data.provider.bdpcommand.Create
-        (
-        'select designator'
-        + ' from fiscal_year'
-        +   ' join state_dictated_appropriation on (state_dictated_appropriation.fiscal_year_id=fiscal_year.id)'
-        +   ' join region_dictated_appropriation'
-        +     ' on (region_dictated_appropriation.state_dictated_appropriation_id=state_dictated_appropriation.id)'
-        + ' where region_dictated_appropriation.id = ' + session.Item['region_dictated_appropriation_id'].tostring,
-        appcommon.db
-        );
-      //   Set up the command to get the County Coorindator's email address.
-      bc_get_cc_email_address := borland.data.provider.bdpcommand.Create
-        (
-        'select password_reset_email_address from county_user where id = ' + session.item['county_user_id'].tostring,
-        appcommon.db
-        );
-      //   Set up the messageText.
-      messageText := 'The ' + session.Item['county_name'].ToString + ' County EMSOF Coordinator has made a new EMSOF appropriation '
-      + 'of ' + amount.tostring('C') + ' to your service for ' + bc_get_fy_designator.ExecuteScalar.tostring + '.' + NEW_LINE
-      + NEW_LINE
-      + 'You can work on this appropriation by visiting:' + NEW_LINE
-      + NEW_LINE
-      + '   http://' + ConfigurationSettings.AppSettings['ssl_base_path'] + '/'
-      + server.UrlEncode(ConfigurationSettings.AppSettings['application_name'])
-      + '/protected/service_overview.aspx' + NEW_LINE
-      + NEW_LINE
-      + 'Replies to this message will be addressed to the ' + session.Item['county_name'].ToString + ' County EMSOF Coordinator.'
-      + NEW_LINE
-      + NEW_LINE
-      + '-- ' + ConfigurationSettings.AppSettings['application_name'];
-      //   Send the email message.
-      smtpmail.SmtpServer := ConfigurationSettings.AppSettings['smtp_server'];
-      smtpmail.Send
-        (
-        bc_get_cc_email_address.ExecuteScalar.tostring,
-        bc_get_service_email_address.ExecuteScalar.tostring,
-        'New ' + ConfigurationSettings.AppSettings['application_name'] + ' appropriation for your service',
-        messageText
-        );
-      //
-      appcommon.DbClose;
-    end;
-  end;
+  appcommon.DbOpen;
+  //
+  // Record the new appropriation.
+  //
+  borland.data.provider.bdpcommand.Create
+    (
+    'insert into county_dictated_appropriation'
+    + ' set region_dictated_appropriation_id = ' + session.Item['region_dictated_appropriation_id'].tostring + ','
+    +   ' service_id = ' + service_id_string + ','
+    +   ' amount = ' + amount.tostring + ','
+    +   ' match_level_id = ' + Safe(RadioButtonList_match_level.selectedvalue,NUM),
+    appcommon.db
+    )
+    .ExecuteNonQuery;
+  //
+  // Initialize a new emsof_request_master record, since at this time there must be a one-to-one relationship between a county-
+  // dictated appropriation and an EMSOF request.
+  //
+  //   Get max(county_dictated_appropriation.id), which must be the id of the county_dictated_appropriation record that we just
+  //   inserted.
+  //
+  max_county_dictated_appropriation_id_string := borland.data.provider.bdpcommand.Create
+    (
+    'select max(id) from county_dictated_appropriation',
+    appcommon.db
+    )
+    .ExecuteScalar.tostring;
+  //
+  //    Insert and link back to the above max id.
+  //
+  borland.data.provider.bdpcommand.Create
+    (
+    'insert into emsof_request_master set county_dictated_appropriation_id = ' + max_county_dictated_appropriation_id_string,
+    appcommon.db
+    )
+    .ExecuteNonQuery;
+  //
+  // Send notice of the appropriation to the service's email address of record.
+  //
+  //   Set up the command to get service's email address of record.
+  bc_get_service_email_address := borland.data.provider.bdpcommand.Create
+    (
+    'select password_reset_email_address from service_user '
+    + 'where id = ' + service_id_string,
+    appcommon.db
+    );
+  //   Set up the command to get the appropriate fiscal year designator.
+  bc_get_fy_designator := borland.data.provider.bdpcommand.Create
+    (
+    'select designator'
+    + ' from fiscal_year'
+    +   ' join state_dictated_appropriation on (state_dictated_appropriation.fiscal_year_id=fiscal_year.id)'
+    +   ' join region_dictated_appropriation'
+    +     ' on (region_dictated_appropriation.state_dictated_appropriation_id=state_dictated_appropriation.id)'
+    + ' where region_dictated_appropriation.id = ' + session.Item['region_dictated_appropriation_id'].tostring,
+    appcommon.db
+    );
+  //   Set up the command to get the County Coorindator's email address.
+  bc_get_cc_email_address := borland.data.provider.bdpcommand.Create
+    (
+    'select password_reset_email_address from county_user where id = ' + session.item['county_user_id'].tostring,
+    appcommon.db
+    );
+  //   Set up the messageText.
+  messageText := 'The ' + session.Item['county_name'].ToString + ' County EMSOF Coordinator has made a new EMSOF appropriation '
+  + 'of ' + amount.tostring('C') + ' to your service for ' + bc_get_fy_designator.ExecuteScalar.tostring + '.' + NEW_LINE
+  + NEW_LINE
+  + 'You can work on this appropriation by visiting:' + NEW_LINE
+  + NEW_LINE
+  + '   http://' + ConfigurationSettings.AppSettings['ssl_base_path'] + '/'
+  + server.UrlEncode(ConfigurationSettings.AppSettings['application_name'])
+  + '/protected/service_overview.aspx' + NEW_LINE
+  + NEW_LINE
+  + 'Replies to this message will be addressed to the ' + session.Item['county_name'].ToString + ' County EMSOF Coordinator.'
+  + NEW_LINE
+  + NEW_LINE
+  + '-- ' + ConfigurationSettings.AppSettings['application_name'];
+  //   Send the email message.
+  smtpmail.SmtpServer := ConfigurationSettings.AppSettings['smtp_server'];
+  smtpmail.Send
+    (
+    bc_get_cc_email_address.ExecuteScalar.tostring,
+    bc_get_service_email_address.ExecuteScalar.tostring,
+    'New ' + ConfigurationSettings.AppSettings['application_name'] + ' appropriation for your service',
+    messageText
+    );
+  //
+  appcommon.DbClose;
 end;
 
 procedure TWebForm_create_new_service_appropriation.Bind_services;
