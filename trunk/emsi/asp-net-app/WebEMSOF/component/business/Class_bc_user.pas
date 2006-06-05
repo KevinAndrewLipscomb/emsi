4e2588ca3beed284c2e897a1e3cc09c0719b878e
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2006-06-05 15:52:45 +0000

    These units help implement role-based authorization.

---
+++ trunk/emsi/asp-net-app/WebEMSOF/component/business/Class_bc_user.pas
@@ -0,0 +1,40 @@
+unit Class_bc_user;
+
+interface
+
+uses
+  appcommon,
+  Class_dalc_user;
+
+type
+  TClass_bc_user = class
+  private
+    { Private Declarations }
+  public
+    constructor Create;
+    function RolesOf(id: string): appcommon.string_array;
+  end;
+
+implementation
+
+const
+  ID_THAT_HAS_ROLES_TOKEN = 'regional-staffer-';
+
+constructor TClass_bc_user.Create;
+begin
+  inherited Create;
+  // TODO: Add any constructor code here
+end;
+
+function TClass_bc_user.RolesOf(id: string): appcommon.string_array;
+begin
+  if id.StartsWith(ID_THAT_HAS_ROLES_TOKEN) then begin
+    RolesOf := Class_dalc_user.TClass_dalc_user.Create.RolesOf
+      (
+      'regional_staffer_',
+      id.Replace(ID_THAT_HAS_ROLES_TOKEN,system.string.EMPTY)
+      );
+  end;
+end;
+
+end.
