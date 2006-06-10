46c8ca73b270db7bd671a52bd50ac028cf6ec5ce
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2006-06-10 14:20:28 +0000

    Added code to display a service's parent appropriation amount, and corrected logic to properly display approval blocks when appropriate.

--- trunk/emsi/asp-net-app/WebEMSOF/component/dal/Class_dalc_appropriations.pas
+++ trunk/emsi/asp-net-app/WebEMSOF/component/dal/Class_dalc_appropriations.pas
@@ -12,17 +12,25 @@ type
     { Private Declarations }
   public
     constructor Create;
+    function AppropriationFromNamedParent
+      (
+      parent_name: string;
+      recipient_kind: string;
+      recipient_id: string;
+      fy_id: string
+      )
+      : decimal;
     function AppropriationFromOnlyParent
       (
-      user_kind: string;
-      user_id: string;
+      recipient_kind: string;
+      recipient_id: string;
       fy_id: string
       )
       : decimal;
     function SumOfSelfDictatedAppropriations
       (
-      user_kind: string;
-      user_id: string;
+      self_kind: string;
+      self_id: string;
       fy_id: string
       )
       : decimal;
@@ -36,28 +44,65 @@ begin
   // TODO: Add any constructor code here
 end;
 
-function TClass_dalc_appropriations.AppropriationFromOnlyParent
+function TClass_dalc_appropriations.AppropriationFromNamedParent
   (
-  user_kind: string;
-  user_id: string;
+  parent_name: string;
+  recipient_kind: string;
+  recipient_id: string;
   fy_id: string
   )
   : decimal;
 var
   cmdText: string;
 begin
-  if user_kind = 'regional-staffer' then begin
-    cmdText := 'select amount'
-      + ' from state_dictated_appropriation'
-      +   ' join regional_staffer on (regional_staffer.region_code=state_dictated_appropriation.region_code)'
-      + ' where regional_staffer.id = ' + user_id
+  if recipient_kind = 'service' then begin
+    cmdText := 'select county_dictated_appropriation.amount'
+      + ' from county_dictated_appropriation'
+      +   ' join region_dictated_appropriation'
+      +     ' on (region_dictated_appropriation.id=county_dictated_appropriation.region_dictated_appropriation_id)'
+      +   ' join county_code_name_map on (county_code_name_map.code=region_dictated_appropriation.county_code)'
+      +   ' join state_dictated_appropriation'
+      +     ' on (state_dictated_appropriation.id=region_dictated_appropriation.state_dictated_appropriation_id)'
+      + ' where service_id = ' + recipient_id
+      +   ' and name = "' + parent_name + '"'
       +   ' and fiscal_year_id = ' + fy_id;
-  end else if user_kind = 'county' then begin
-    cmdText := 'select amount'
+  end else if recipient_kind = 'county' then begin
+    cmdText := 'select region_dictated_appropriation.amount'
       + ' from region_dictated_appropriation'
       +   ' join state_dictated_appropriation'
-      +     'on (state_dictated_appropriation.region_code=region_dictated_appropriation.state_dictated_appropriation_id)'
-      + ' where county_code = ' + user_id
+      +     ' on (state_dictated_appropriation.region_code=region_dictated_appropriation.state_dictated_appropriation_id)'
+      +   ' join region_code_name_map on (region_code_name_map.code=state_dictated_appropriation.region_code)'
+      + ' where county_code = ' + recipient_id
+      +   ' and name = "' + parent_name + '"'
+      +   ' and fiscal_year_id = ' + fy_id;
+  end;
+  connection.Open;
+  AppropriationFromNamedParent := decimal(borland.data.provider.bdpcommand.Create(cmdText,connection).ExecuteScalar);
+  connection.Close;
+end;
+
+function TClass_dalc_appropriations.AppropriationFromOnlyParent
+  (
+  recipient_kind: string;
+  recipient_id: string;
+  fy_id: string
+  )
+  : decimal;
+var
+  cmdText: string;
+begin
+  if recipient_kind = 'regional-staffer' then begin
+    cmdText := 'select state_dictated_appropriation.amount'
+      + ' from state_dictated_appropriation'
+      +   ' join regional_staffer on (regional_staffer.region_code=state_dictated_appropriation.region_code)'
+      + ' where regional_staffer.id = ' + recipient_id
+      +   ' and fiscal_year_id = ' + fy_id;
+  end else if recipient_kind = 'county' then begin
+    cmdText := 'select region_dictated_appropriation.amount'
+      + ' from region_dictated_appropriation'
+      +   ' join state_dictated_appropriation'
+      +     ' on (state_dictated_appropriation.region_code=region_dictated_appropriation.state_dictated_appropriation_id)'
+      + ' where county_code = ' + recipient_id
       +   ' and fiscal_year_id = ' + fy_id;
   end;
   connection.Open;
@@ -67,30 +112,30 @@ end;
 
 function TClass_dalc_appropriations.SumOfSelfDictatedAppropriations
   (
-  user_kind: string;
-  user_id: string;
+  self_kind: string;
+  self_id: string;
   fy_id: string
   )
   : decimal;
 var
   cmdText: string;
 begin
-  if user_kind = 'regional-staffer' then begin
+  if self_kind = 'regional-staffer' then begin
     cmdText := 'select sum(region_dictated_appropriation.amount)'
     + ' from region_dictated_appropriation'
     +   ' join state_dictated_appropriation'
     +     ' on (state_dictated_appropriation.id=region_dictated_appropriation.state_dictated_appropriation_id)'
     +   ' join regional_staffer on (regional_staffer.region_code=state_dictated_appropriation.region_code)'
-    + ' where regional_staffer.id = ' + user_id
+    + ' where regional_staffer.id = ' + self_id
     +   ' and fiscal_year_id = ' + fy_id;
-  end else if user_kind = 'county' then begin
+  end else if self_kind = 'county' then begin
     cmdText := 'select sum(county_dictated_appropriation.amount)'
     + ' from county_dictated_appropriation'
     +   ' join region_dictated_appropriation'
     +     ' on (region_dictated_appropriation.id=county_dictated_appropriation.region_dictated_appropriation_id)'
     +   ' join state_dictated_appropriation'
     +     ' on (state_dictated_appropriation.id=region_dictated_appropriation.state_dictated_appropriation_id)'
-    + ' where county_code = ' + user_id
+    + ' where county_code = ' + self_id
     +   ' and fiscal_year_id = ' + fy_id;
   end;
   connection.Open;
