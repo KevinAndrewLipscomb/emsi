628e58fbfe1489bcb84bed94cf4a856d632978ce
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2006-07-22 21:09:39 +0000

    Creation.

---
+++ asp-net-app/WebEMSOF/component/biz/Class_biz_regions.pas
@@ -0,0 +1,31 @@
+unit Class_biz_regional_staffers;
+
+interface
+
+uses
+  Class_db_regional_staffers;
+
+type
+  TClass_biz_regional_staffers = class
+  private
+    db_regional_staffers: TClass_db_regional_staffers;
+  public
+    constructor Create;
+    function RegionCodeOf(id: string): string;
+  end;
+
+implementation
+
+constructor TClass_biz_regional_staffers.Create;
+begin
+  inherited Create;
+  // TODO: Add any constructor code here
+  db_regional_staffers := TClass_db_regional_staffers.Create;
+end;
+
+function TClass_biz_regional_staffers.RegionCodeOf(id: string): string;
+begin
+  RegionCodeOf := db_regional_staffers.RegionCodeOf(id);
+end;
+
+end.
