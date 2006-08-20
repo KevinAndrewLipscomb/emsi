8086dfa4b6c8319ad485ea170e78301cdfad3d45
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2006-08-20 15:36:48 +0000

    Changed work.bat to open the X IIS Application instance.  Added SQL insertion safety to all uses of the DropDownList_amendment_num in state_required_report.

--- trunk/emsi/asp-net-app/WebEMSOF/work.bat
+++ trunk/emsi/asp-net-app/WebEMSOF/work.bat
@@ -4,4 +4,4 @@ REM
 cd "C:\Inetpub\wwwroot\WebEMSOF"
 start /max WebEMSOF.bdsproj
 start /max explorer /e,/select,C:\Inetpub\wwwroot\WebEMSOF\.svn
-start /max ftp://ftp.frompaper2web.com/ssl/WebEMSOF
+start /max ftp://ftp.frompaper2web.com/ssl/WebEMSOF_x
