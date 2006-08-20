2ed3f6b231f2664c5bb714e310517af931a656a3
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2006-08-20 18:00:38 +0000

    Revised to start MySqlQueryBrowser.

--- trunk/emsi/asp-net-app/WebEMSOF/work.bat
+++ trunk/emsi/asp-net-app/WebEMSOF/work.bat
@@ -2,6 +2,7 @@ REM
 REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\WebEMSOF"
-start /max WebEMSOF.bdsproj
 start /max explorer /e,/select,C:\Inetpub\wwwroot\WebEMSOF\.svn
 start /max ftp://ftp.frompaper2web.com/ssl/WebEMSOF_x
+start /max WebEMSOF.bdsproj
+start "" /max "C:\Program Files\MySQL\MySQL Query Browser 1.1\MySQLQueryBrowser.exe"
