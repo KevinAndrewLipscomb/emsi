6112971851393740e9ed3c4639c9618446e59739
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2007-03-31 22:56:21 +0000

    Revised to use robocopy instead of xcopy, and to operate on new standard folders.

--- trunk/emsi/asp-net-app/WebEMSOF/deploy.bat
+++ trunk/emsi/asp-net-app/WebEMSOF/deploy.bat
@@ -2,19 +2,23 @@
 @REM $Id$
 @REM
 @rmdir ready-to-deploy /s /q
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\*.asax ready-to-deploy /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\*.aspx ready-to-deploy /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\*.config ready-to-deploy /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\*.pdf ready-to-deploy /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\bin\*.* ready-to-deploy\bin /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\noninteractive\*.crontab ready-to-deploy\noninteractive /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\noninteractive\*.aspx ready-to-deploy\noninteractive /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\protected\*.asax ready-to-deploy\protected /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\protected\*.aspx ready-to-deploy\protected /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\protected\*.config ready-to-deploy\protected /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\protected\image ready-to-deploy\protected\image /m /i /k /y /z
-@xcopy C:\Inetpub\wwwroot\WebEMSOF\resource ready-to-deploy\resource /m /i /k /y /z
-@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Data.Provider.dll" ready-to-deploy\bin /m /i /k /y /z
-@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Data.Common.dll" ready-to-deploy\bin /m /i /k /y /z
-@xcopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0\Borland.Delphi.dll" ready-to-deploy\bin /m /i /k /y /z
-@xcopy "C:\Program Files\CoreLab\Bdp\MySql\BDS4\CoreLab.Bdp.MySql.dll" ready-to-deploy\bin /m /i /k /y /z
+@REM-cmd--sourcedir----------------------------------------------------------------destdir-----------------------------------------[files]-------------------qualifiers
+@robocopy C:\Inetpub\wwwroot\WebEMSOF                                              ready-to-deploy                                 *.asax                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF                                              ready-to-deploy                                 *.aspx                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF                                              ready-to-deploy                                 *.config                  /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF                                              ready-to-deploy                                 *.pdf                     /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\bin                                          ready-to-deploy\bin                                                       /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\noninteractive                               ready-to-deploy\noninteractive                  *.crontab                 /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\noninteractive                               ready-to-deploy\noninteractive                  *.aspx                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\protected                                    ready-to-deploy\protected                       *.asax                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\protected                                    ready-to-deploy\protected                       *.aspx                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\protected                                    ready-to-deploy\protected                       *.config                  /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\protected\image                              ready-to-deploy\protected\image                                           /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\protected\template\notification              ready-to-deploy\protected\template\notification                           /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\resource                                     ready-to-deploy\resource                                                  /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\usercontrol\app                              ready-to-deploy\usercontrol\app                 *.ascx                    /m /np /z
+@robocopy C:\Inetpub\wwwroot\WebEMSOF\usercontrol\ki                               ready-to-deploy\usercontrol\ki                  *.ascx                    /A /np /z
+@robocopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0" ready-to-deploy\bin                             Borland.Data.Provider.dll /m /np /z
+@robocopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0" ready-to-deploy\bin                             Borland.Data.Common.dll   /m /np /z
+@robocopy "C:\Program Files\Common Files\Borland Shared\BDS\Shared Assemblies\4.0" ready-to-deploy\bin                             Borland.Delphi.dll        /m /np /z
+@robocopy "C:\Program Files\CoreLab\Bdp\MySql\BDS4"                                ready-to-deploy\bin                             CoreLab.Bdp.MySql.dll     /m /np /z
