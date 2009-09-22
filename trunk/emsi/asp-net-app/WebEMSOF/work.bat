7c936ffc2b706cfc91cde21d5f778a88a2b78d80
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-09-22 19:57:21 +0000

    Revised to open SLN file instead of DPROJ file.

--- trunk/emsi/asp-net-app/WebEMSOF/work.bat
+++ trunk/emsi/asp-net-app/WebEMSOF/work.bat
@@ -5,5 +5,5 @@ cd "C:\Inetpub\wwwroot\WebEMSOF"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\WebEMSOF\.svn
 start /max explorer ftp://ftp.frompaper2web.com/ssl/WebEMSOF_x
 start /max explorer ftp://frompaper2web.win.aplus.net/WWWROOT/WebEMSOF/
-start /max WebEMSOF.dproj
+start /max WebEMSOF.sln
 start "" /max "C:\Program Files\MySQL\MySQL Tools for 5.0\MySQLQueryBrowser.exe"
