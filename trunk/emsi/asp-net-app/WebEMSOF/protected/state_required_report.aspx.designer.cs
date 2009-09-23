937a7996faed824a881fc786e76c204b8bcbe712
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-09-23 17:45:14 +0000

    Alpha work to get C# .NET Framework 3.5 version running in X instance.

--- trunk/emsi/asp-net-app/WebEMSOF/protected/state_required_report.aspx.designer.cs
+++ trunk/emsi/asp-net-app/WebEMSOF/protected/state_required_report.aspx.designer.cs
@@ -20,7 +20,7 @@ namespace state_required_report {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Title;
+        protected new global::System.Web.UI.HtmlControls.HtmlGenericControl Title;
         
         /// <summary>
         /// UserControl_precontent control.
