399fc6263c9a647f3ed75e913581aa5fbb5ee7c5
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2009-11-27 15:36:47 +0000

    Added code to protect, by reducing to a null-op, against crash whose root cause is undetermined.

--- trunk/emsi/asp-net-app/WebEMSOF/protected/finalize.aspx.designer.cs
+++ trunk/emsi/asp-net-app/WebEMSOF/protected/finalize.aspx.designer.cs
@@ -14,15 +14,6 @@ namespace finalize {
     public partial class TWebForm_finalize {
         
         /// <summary>
-        /// Title control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Title;
-        
-        /// <summary>
         /// UserControl_precontent control.
         /// </summary>
         /// <remarks>
