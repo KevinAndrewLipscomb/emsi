ada627ce894be298a1b4f0b7c59206c5b0c7968c
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2010-05-26 20:10:23 +0000

    Revised to use built-in FTP instead of WinSCP.

--- asp-net-app/WebEMSOF/work.bat
+++ asp-net-app/WebEMSOF/work.bat
@@ -3,7 +3,7 @@ REM $Id$
 REM
 cd "C:\Inetpub\wwwroot\WebEMSOF"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\WebEMSOF\.svn
-start "" /max "C:\Program Files\WinSCP\WinSCP.exe" alentus.com/ssl/WebEMSOF_x
-start "" /max "C:\Program Files\WinSCP\WinSCP.exe" aplus.net/WebEMSOF/
+start /max explorer ftp://kalipso:@frompaper2web.com/ssl/WebEMSOF_x
+start /max explorer ftp://frompaper2web:@frompaper2web.win.aplus.net/WWWROOT/WebEMSOF/
 start /max WebEMSOF.sln
 start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 OSS\MySQLWorkbench.exe"
