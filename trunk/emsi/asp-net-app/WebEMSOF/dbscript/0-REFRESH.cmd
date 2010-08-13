d6e5893bd00e175e3c66f645620469f2d97ddcd6
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2010-08-13 20:43:09 +0000

    Initial control.

---
+++ trunk/emsi/asp-net-app/WebEMSOF/dbscript/0-REFRESH.cmd
@@ -0,0 +1,11 @@
+@REM $Id$
+@REM
+set the_host=localhost
+set the_user=root
+if "%1"=="x" set the_host=elm.phpwebhosting.com
+if "%1"=="x" set the_user=kalipso5
+echo on
+mysql --host=%the_host% --user=%the_user% --password=%2 --execute="drop schema emsidb_%1"
+mysql --host=%the_host% --user=%the_user% --password=%2 --execute="create schema emsidb_%1"
+mysql --host=%the_host% --user=%the_user% --password=%2 --database=emsidb_%1 <"%USERPROFILE%\My Documents\SANDBOX\vocational\kalipso-infogistics\db-dump\emsidb.sql"
+mysql --host=%the_host% --user=%the_user% --password=%2 --database=emsidb_%1 <0-render-safe-for-d-or-x.sql
