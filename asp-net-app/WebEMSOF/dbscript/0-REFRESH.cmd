9e3b9d044c8dbe4c826bf5c95148e0efe96b37cc
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2010-08-19 18:40:18 +0000

    Revised to work on WinXP as well as Win7 by deferring to a junction point.

--- asp-net-app/WebEMSOF/dbscript/0-REFRESH.cmd
+++ asp-net-app/WebEMSOF/dbscript/0-REFRESH.cmd
@@ -7,5 +7,5 @@ if "%1"=="x" set the_user=kalipso5
 echo on
 mysql --host=%the_host% --user=%the_user% --password=%2 --execute="drop schema emsidb_%1"
 mysql --host=%the_host% --user=%the_user% --password=%2 --execute="create schema emsidb_%1"
-mysql --host=%the_host% --user=%the_user% --password=%2 --database=emsidb_%1 <"%USERPROFILE%\My Documents\SANDBOX\vocational\kalipso-infogistics\db-dump\emsidb.sql"
+mysql --host=%the_host% --user=%the_user% --password=%2 --database=emsidb_%1 <"%USERPROFILE%\my-documents\SANDBOX\vocational\kalipso-infogistics\db-dump\emsidb.sql"
 mysql --host=%the_host% --user=%the_user% --password=%2 --database=emsidb_%1 <0-render-safe-for-d-or-x.sql
