d9c0582dfcbab3496239e70750b993d9b9f8b8a9
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2010-11-10 18:25:55 +0000

    Removed reference to defunct server account.

--- asp-net-app/WebEMSOF/work.bat
+++ asp-net-app/WebEMSOF/work.bat
@@ -4,6 +4,5 @@ REM
 cd "C:\Inetpub\wwwroot\WebEMSOF"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\WebEMSOF\.svn
 start /max explorer ftp://kalipso:@frompaper2web.com/ssl/WebEMSOF_x
-start /max explorer ftp://frompaper2web:@frompaper2web.win.aplus.net/WWWROOT/WebEMSOF/
 start /max WebEMSOF.sln
 start "" /max "C:\Program Files\MySQL\MySQL Workbench 5.2 CE\MySQLWorkbench.exe"
