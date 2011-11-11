7e2bdec6254d1623afe9f40d5b056d4572072b33
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2011-11-11 22:30:56 +0000

    Alpha work on EMSRS Gateway feature.

--- asp-net-app/WebEMSOF/component/biz/Class_biz_scheduled_tasks.cs
+++ asp-net-app/WebEMSOF/component/biz/Class_biz_scheduled_tasks.cs
@@ -1,20 +1,28 @@
+using Class_biz_coned_offerings;
 using WebEMSOF.component.os;
 
 namespace Class_biz_scheduled_tasks
   {
   public class TClass_biz_scheduled_tasks
     {
+    private TClass_biz_coned_offerings biz_coned_offerings;
     private Class_fs fs;
 
     //Constructor  Create()
     public TClass_biz_scheduled_tasks() : base()
       {
+      biz_coned_offerings = new TClass_biz_coned_offerings();
       fs = new Class_fs();
       }
 
     public void DoDailyChores(string current_working_directory_spec)
       {
       fs.DeleteCondemnedFolders(current_working_directory_spec + "/../protected/attachment");
+      }
+
+    public void DoQ20mChores()
+      {
+      biz_coned_offerings.RetreiveLatestMasterRecords();
       }
 
     } // end TClass_biz_scheduled_tasks
