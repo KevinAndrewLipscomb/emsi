0115ae3f3c4de2be1b0ca3b2586101bde83f0748
KevinAnLipscomb <KevinAnLipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2012-04-09 16:33:41 +0000

    Revised to be a bit more aggressive.

--- trunk/emsi/asp-net-app/ConEdLink/dbscript/2012-04-09-A--remove-stale-unused-classes.sql
+++ trunk/emsi/asp-net-app/ConEdLink/dbscript/2012-04-09-A--remove-stale-unused-classes.sql
@@ -1,5 +1,5 @@
 START TRANSACTION
 ;
-delete from coned_offering where end_date_time < "2012-01-28" and (select count(*) from coned_offering_roster where coned_offering_id = coned_offering.id) = 0
+delete from coned_offering where end_date_time < "2012-02-09" and (select count(*) from coned_offering_roster where coned_offering_id = coned_offering.id) = 0
 ;
 COMMIT
