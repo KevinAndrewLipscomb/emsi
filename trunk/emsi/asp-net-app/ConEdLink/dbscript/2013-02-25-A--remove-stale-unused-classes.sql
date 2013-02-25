e33e2526eeabd656302e808cfebd09e2912f6a92
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2013-02-25 21:12:14 +0000

    Creation.

---
+++ trunk/emsi/asp-net-app/ConEdLink/dbscript/2013-02-25-A--remove-stale-unused-classes.sql
@@ -0,0 +1,5 @@
+START TRANSACTION
+;
+delete from coned_offering where end_date_time < "2012-12-25" and (select count(*) from coned_offering_roster where coned_offering_id = coned_offering.id) = 0
+;
+COMMIT
