9654e5af2b0c76ebe2d8c89fd930851458359661
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2013-04-04 17:41:59 +0000

    Revised to more generally reference MySQL Connector .NET (coincident with upgrading that component).  Revised 1-user-setup.sql templates to account for an interpreter quirk full comment lines.  Added Bradley Takach as EMSI regional staffer.  Revised work.bat to account for Windows security issue that prevents a more general reference involving a junction point.

--- asp-net-app/ConEdLink/work.bat
+++ asp-net-app/ConEdLink/work.bat
@@ -4,4 +4,4 @@ REM
 cd "C:\Inetpub\wwwroot\ConEdLink"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\ConEdLink\.svn
 start /max ConEdLink.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
+start "" /max "C:\Program Files\MySQL\MySQL Workbench CE 5.2.47\MySQLWorkbench.exe"
