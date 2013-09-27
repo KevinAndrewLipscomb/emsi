904a23fedb7be5404bae07fad605392b4929005e
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2013-09-27 14:52:22 +0000

    Creation.

---
+++ trunk/emsi/asp-net-app/ConEdLink/dbscript/2013-09-26-A--update-eastern-staff.sql
@@ -0,0 +1,14 @@
+START TRANSACTION
+;
+insert ignore regional_staffer (region_code,last_name,first_name) values
+((select code from region_code_name_map where name = "Eastern"),"Garrity","Lisa")
+;
+insert ignore regional_staffer_user (id,password_reset_email_address) values
+((select id from regional_staffer where first_name = "Lisa" and last_name = "Garrity"),"lisagarrity@easternemscouncil.org")
+;
+insert ignore regional_staffer_role (group_id,user_id) values
+((select id from regional_staffer_group where name = "education-specialist"),(select id from regional_staffer_user where password_reset_email_address = "lisagarrity@easternemscouncil.org"))
+;
+update regional_staffer_user set be_active = FALSE where id = 9
+;
+COMMIT
