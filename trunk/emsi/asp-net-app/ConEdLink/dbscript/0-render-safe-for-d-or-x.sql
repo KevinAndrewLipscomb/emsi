e9eb4e111f5dd06078c71708b8160a722994a8b9
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-02-10 20:45:49 +0000

    Revised to make user password_reset_email_address values safe.

--- trunk/emsi/asp-net-app/ConEdLink/dbscript/0-render-safe-for-d-or-x.sql
+++ trunk/emsi/asp-net-app/ConEdLink/dbscript/0-render-safe-for-d-or-x.sql
@@ -9,6 +9,9 @@ set password_reset_email_address = concat("RegionalStaffer",id,"@frompaper2web.c
 update service_user
 set password_reset_email_address = concat("Service",id,"@frompaper2web.com")
 ;
+update user
+set password_reset_email_address = concat(username,"@frompaper2web.com");
+;
 update service
 set corpadmin_email_address = concat("Service",id,"_Corpadmin@frompaper2web.com")
 , coo_email_address = concat("Service",id,"_Coo@frompaper2web.com")
