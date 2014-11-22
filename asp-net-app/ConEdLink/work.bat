1ef5ee74432feffdd36e181d2d072301338ed021
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2014-11-22 22:37:03 +0000

    Revised to work on both Win32 and Win64.

--- asp-net-app/ConEdLink/work.bat
+++ asp-net-app/ConEdLink/work.bat
@@ -4,4 +4,4 @@ REM
 cd "C:\Inetpub\wwwroot\ConEdLink"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\ConEdLink\.svn
 start /max ConEdLink.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
+IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
