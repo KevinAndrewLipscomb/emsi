7115b42febba537a325605dd123814ec41fc7b14
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2015-08-18 19:45:12 +0000

    Added more xmlns phrases per suggestions found on the interwebs.

--- trunk/emsi/asp-net-app/PACRAT/usercontrol/app/UserControl_strike_team_deployment_snapshot.ascx.cs
+++ trunk/emsi/asp-net-app/PACRAT/usercontrol/app/UserControl_strike_team_deployment_snapshot.ascx.cs
@@ -327,7 +327,7 @@ namespace UserControl_strike_team_deployment_snapshot
         );
       var raw_xml = k.EMPTY
         + "<?xml version=\"1.0\"?>" + k.NEW_LINE
-        + "<ss:Workbook xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\">" + k.NEW_LINE
+        + "<ss:Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\">" + k.NEW_LINE
         +   "<ss:Worksheet ss:Name=\"Intro\">" + k.NEW_LINE
         +     "<ss:Table>" + k.NEW_LINE
         +       "<ss:Row><ss:Cell><ss:Data ss:Type=\"String\">This is a snapshot of data associated with the '" + name + "' deployment.</ss:Data></ss:Cell></ss:Row>" + k.NEW_LINE
