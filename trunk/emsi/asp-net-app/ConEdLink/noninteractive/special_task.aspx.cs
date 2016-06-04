c788d16156fe9358dd8ab520e85c98099cfab660
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2016-06-04 17:39:38 +0000

    Revised MimicTraditionalInsertOnDuplicateKeyUpdate to gracefully handle anomalous case of finding more than one row that matches a given (and presumed unique) set of match conditions.

--- trunk/emsi/asp-net-app/ConEdLink/noninteractive/special_task.aspx.cs
+++ trunk/emsi/asp-net-app/ConEdLink/noninteractive/special_task.aspx.cs
@@ -1,4 +1,3 @@
-using Class_biz_old_analysis_of_region_by_years;
 using System;
 
 namespace special_task
@@ -6,26 +5,25 @@ namespace special_task
   public partial class TWebForm_special_task: System.Web.UI.Page
     {
 
-        // / <summary>
-        // / Required method for Designer support -- do not modify
-        // / the contents of this method with the code editor.
-        // / </summary>
-        private void InitializeComponent()
-        {
-        }
+    // / <summary>
+    // / Required method for Designer support -- do not modify
+    // / the contents of this method with the code editor.
+    // / </summary>
+    private void InitializeComponent()
+      {
+      }
 
-        protected void Page_Load(object sender, System.EventArgs e)
-        {
-        new TClass_biz_old_analysis_of_region_by_years().PopulatePast();
-        }
+    protected void Page_Load(object sender, EventArgs e)
+      {
+      }
 
-        protected override void OnInit(EventArgs e)
-        {
-            // Required for Designer support
-            InitializeComponent();
-            base.OnInit(e);
-        }
+    protected override void OnInit(EventArgs e)
+      {
+      // Required for Designer support
+      InitializeComponent();
+      base.OnInit(e);
+      }
 
-    } // end TWebForm_special_task
+  } // end TWebForm_special_task
 
 }
