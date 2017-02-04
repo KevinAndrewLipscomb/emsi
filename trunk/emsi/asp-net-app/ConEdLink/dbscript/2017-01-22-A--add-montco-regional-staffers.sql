5c2b43a8c622b0fc8ac249e5cbe34c5c274da5b6
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2017-02-04 21:54:50 +0000

    Added Executive Director.

--- trunk/emsi/asp-net-app/ConEdLink/dbscript/2017-01-22-A--add-montco-regional-staffers.sql
+++ trunk/emsi/asp-net-app/ConEdLink/dbscript/2017-01-22-A--add-montco-regional-staffers.sql
@@ -1,16 +1,19 @@
 START TRANSACTION
 ;
 insert ignore regional_staffer (region_code,last_name,first_name) values
+((select code from region_code_name_map where name = "Montgomery County"),"Brown","Dave"),
 ((select code from region_code_name_map where name = "Montgomery County"),"DeShon","Margaret"),
 ((select code from region_code_name_map where name = "Montgomery County"),"Schulberger","Susan"),
 ((select code from region_code_name_map where name = "Montgomery County"),"Pasquale","Brian")
 ;
 insert ignore regional_staffer_user (id,password_reset_email_address) values
+((select id from regional_staffer where first_name = "Dave" and last_name = "Brown"),"dbrown@montcopa.org"),
 ((select id from regional_staffer where first_name = "Margaret" and last_name = "DeShon"),"Mdeshon@montcopa.org"),
 ((select id from regional_staffer where first_name = "Susan" and last_name = "Schulberger"),"SSchulbe@montcopa.org"),
 ((select id from regional_staffer where first_name = "Brian" and last_name = "Pasquale"),"Bpasqual@montcopa.org")
 ;
 insert ignore regional_staffer_role (group_id,user_id) values
+((select id from regional_staffer_group where name = "director"),(select id from regional_staffer_user where password_reset_email_address = "dbrown@montcopa.org")),
 ((select id from regional_staffer_group where name = "education-coordinator"),(select id from regional_staffer_user where password_reset_email_address = "Mdeshon@montcopa.org")),
 ((select id from regional_staffer_group where name = "education-reservist"),(select id from regional_staffer_user where password_reset_email_address = "SSchulbe@montcopa.org")),
 ((select id from regional_staffer_group where name = "education-specialist"),(select id from regional_staffer_user where password_reset_email_address = "Bpasqual@montcopa.org"))
