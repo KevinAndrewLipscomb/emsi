abaa7095e97d59e3a4b961e017e608a0cdabcaaf
Kevin Andrew Lipscomb <kevinandrewlipscomb@vt.edu>
2017-02-09 23:52:13 +0000

    Creation.

---
+++ asp-net-app/ConEdLink/dbscript/2017-02-09-A--add-eastern-regional-staffer.sql
@@ -0,0 +1,12 @@
+START TRANSACTION
+;
+insert ignore regional_staffer (region_code,last_name,first_name) values
+((select code from region_code_name_map where name = "Eastern"),"Melendez","Rachel")
+;
+insert ignore regional_staffer_user (id,password_reset_email_address) values
+((select id from regional_staffer where first_name = "Rachel" and last_name = "Melendez"),"rachel@easternemscouncil.org")
+;
+insert ignore regional_staffer_role (group_id,user_id) values
+((select id from regional_staffer_group where name = "education-coordinator"),(select id from regional_staffer_user where password_reset_email_address = "rachel@easternemscouncil.org"))
+;
+COMMIT
