b4f87b73f4dad95a0b41a846ee0dfa8315ccdb6c
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2017-08-25 18:02:22 +0000

    Creation.

---
+++ trunk/emsi/asp-net-app/ConEdLink/dbscript/2017-08-25-A--add-ehsf-regional-staffer.sql
@@ -0,0 +1,12 @@
+START TRANSACTION
+;
+insert ignore regional_staffer (region_code,last_name,first_name) values
+((select code from region_code_name_map where name = "Federation"),"Carmody","Diana")
+;
+insert ignore regional_staffer_user (id,password_reset_email_address) values
+((select id from regional_staffer where first_name = "Diana" and last_name = "Carmody"),"dcarmody@ehsf.org")
+;
+insert ignore regional_staffer_role (group_id,user_id) values
+((select id from regional_staffer_group where name = "education-reservist"),(select id from regional_staffer_user where password_reset_email_address = "dcarmody@ehsf.org"))
+;
+COMMIT
